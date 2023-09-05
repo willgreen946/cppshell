64e7f05280ca51f307f5d3c7fba686b812d56c5d
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-09-05 21:26:45 +0100

    Cleaned the code abit + cd command working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 /* my attempt at creating a shell in C++ with some C */
 
 #include <iostream>
+#include <vector>
 #include <sstream>
+#include <fstream>
 #include <cstring>
 #include <unistd.h>
 #include <dirent.h>
@@ -13,18 +15,6 @@
 
 namespace config
 {
-	void make_prompt (std::string& str)
-	{
-	}
-
-	std::string vars[256];
-}
-
-namespace cli
-{
-	void exit (char *argv[]);
-	void prev_cmd (char *argv[]);
-
 	/* holds info about the user */
 	typedef struct {
 		char *username; // username
@@ -38,42 +28,91 @@ namespace cli
 		char *home; // home directory
 		time_t expire; // account expiration 
 		int fields; // internal fields	
-	}INFO;
+	}USR_INFO;
 
-	INFO *info;
+	typedef struct {
+		std::string path;
+		std::string home;
+		std::string prompt;
+		std::vector<std::string> usr_vars;
+	}VARS;
 
-	struct SHELL_CMDS {
-		const char *cmd;
-		void (*fn)(char **argv);
-	};
+	USR_INFO *usr_info;
+	VARS vars;
 
-	/* the built in shell commands */
-	struct SHELL_CMDS shell_cmds[] = {
-		{ "export", nullptr },
-		{ "exit", cli::exit },
-		{ "!!", cli::prev_cmd },
-	};
-
-	/* gets data about the user */
-	static void get_info (bool first)
+	/* gets data about the user from pwd */
+	void get_info (bool first)
 	{
 		struct passwd *pwd;
 
 		if (first == true)
-			info = new INFO[sizeof(*info)];
+			config::usr_info = new USR_INFO[sizeof(*config::usr_info)];
 
 		/* get uid */
-		info->uid = getuid();
+		config::usr_info->uid = getuid();
 
 		/* getting user information */
-		pwd = getpwuid(info->uid);
+		pwd = getpwuid(config::usr_info->uid);
 
 		/* setting user information */
-		info->username = strndup(pwd->pw_name, strlen(pwd->pw_name));
-		info->password = strndup(pwd->pw_passwd, strlen(pwd->pw_passwd));
-		info->home = strndup(pwd->pw_dir, strlen(pwd->pw_dir));
+		config::usr_info->username = strndup(pwd->pw_name, 256);
+		config::usr_info->password = strndup(pwd->pw_passwd, 256);
+		config::usr_info->home = strndup(pwd->pw_dir, 256);
 	}
 
+	/* makes the config file path from the username and the home dir */
+	void make_path (void)
+	{
+		constexpr char *conf = (char*) "/.cppshell.conf";
+		char *path = new char [sizeof(config::usr_info->home) + strlen(conf)];
+
+		strncat(config::usr_info->home, conf, strlen(conf));
+
+		config::vars.path = path;
+
+		delete[] path;
+	}
+
+	/* sets the prompt from the config */
+	void set_prompt (void)
+	{
+		std::ifstream file;
+		std::string line, prompt, tmp;
+
+		file.open(config::vars.path, std::ios::out);
+
+		/* if no file use the default prompt */
+		if (!file.is_open()) {
+			config::vars.prompt = "# ";
+			return;
+		}
+
+		/* read until it finds the string PROMPT */
+		for (int i = 0; std::getline(file, line); i++) {
+			/* get first word of string */
+			std::istringstream iss(line);
+			std::getline(iss, tmp, ' ');
+
+			/* get second and scan it */
+			if (tmp == "PROMPT")
+				std::getline(iss, tmp, ' ');
+		}
+
+		file.close();
+		config::vars.prompt = prompt;
+	}
+
+	/* sets up variables from the config file */
+	void setup (bool first)
+	{
+		config::get_info(first);
+		config::make_path();
+		config::set_prompt();
+	}
+}
+
+namespace cmd
+{
 	void prev_cmd (char **argv)
 	{
 
@@ -81,11 +120,37 @@ namespace cli
 
 	void exit (char **argv)
 	{
-		delete[] info;
 		exit(0);
 	}
 
-	/* returns index of the matching command in shell_cmds struct */
+	/* implementation of the cd command */
+	void cd (char **argv)
+	{
+		if (chdir(argv[1]) != 0)
+			std::cerr << argv[1] << ": No such directory" << std::endl;
+	}
+}
+
+namespace cli
+{
+	void exit (char *argv[]);
+	void prev_cmd (char *argv[]);
+
+	struct SHELL_CMDS {
+		const char *cmd;
+		void (*fn)(char **argv);
+	};
+
+	/* the built in shell commands 
+	 * Ones at top are 'most used' */
+	struct SHELL_CMDS shell_cmds[] = {
+		{ "cd", cmd::cd },
+		{ "export", nullptr },
+		{ "exit", cmd::exit },
+		{ "!!", cmd::prev_cmd },
+	};
+
+		/* returns index of the matching command in shell_cmds struct */
 	static ssize_t is_built_in (char *cmd)
 	{
 		for (size_t i = 0; shell_cmds[i].cmd != nullptr; i++)
@@ -108,24 +173,52 @@ namespace cli
 					}
 	}
 
+	/* runs a command */
+	int run_cmd (int argc, char **argv)
+	{
+		int ret, status;
+		pid_t pid, ppid;
+
+		if (argv[0] == nullptr)
+			return 0; // Do nothing 
+
+		/* check for shell commands */
+		else if ((ret = is_built_in(strdup(argv[0]))) != -1)
+			shell_cmds[ret].fn(argv);
+
+		/* if argv[0] is not a shell command then execute a binary */
+		else {
+			/* fork the process and execute the command */
+			pid = fork();
+
+			if (pid == 0) {
+				execvp(argv[0], argv);
+				std::cerr << argv[0] << ": Unkown Command" << std::endl;
+			}
+
+			else
+				do {
+					ppid = wait(&status);
+					if (ppid != pid) break;
+			} while (ppid != pid);
+		}
+
+		return 0;
+	}
+
 	/* the entry point for the cli */
 	void entry (void)
 	{
 		constexpr size_t MAX_LEN = 256;
-		char *argv[MAX_LEN];
 		int argc;
-		int status;
-		ssize_t ret;
-		pid_t pid;
-		pid_t ppid;
-		std::string input;
-		std::string tmp;
+		char *argv[MAX_LEN];
+		std::string input, tmp;
 
-		cli::get_info(true);
+		config::setup(true);
 
 		/* the main loop of the program */
 		while (true) {
-			std::cout << "[" << info->username << "]" << "# ";
+			std::cout << config::vars.prompt;
 			std::getline(std::cin, input);
 
 			/* splitting string into command and argv */
@@ -141,34 +234,9 @@ namespace cli
 				}
 			}
 
-			if (argv[0] == nullptr)
-				; // Do nothing 
-
-			/* check for shell commands */
-			else if ((ret = is_built_in(argv[0])) != -1)
-				shell_cmds[ret].fn(argv);
-
-			/* if argv[0] is not a shell command then execute a binary */
-			else {
-				/* fork the process and execute the command */
-				pid = fork();
-
-				if (pid == 0) {
-					execvp(argv[0], argv);
-					std::cerr << argv[0] << ": Unkown Command" << std::endl;
-				}
-
-				else
-					do {
-						ppid = wait(&status);
-						if (ppid != pid) break;
-					} while (ppid != pid);
-			}
+			cli::run_cmd(argc, argv);
 
 			/* clear the values */
-			input.clear();
-			tmp.clear();
-
 			for (size_t i = 0; i < argc; i++)
 				argv[i] = nullptr;
 		}
